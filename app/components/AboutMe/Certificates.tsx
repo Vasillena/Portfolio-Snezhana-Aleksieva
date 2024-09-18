db91876eb5eac76d3b2b16a1a978c4bcf8d991f8
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-09-18 14:34:17 +0300

    V

--- app/components/AboutMe/Certificates.tsx
+++ app/components/AboutMe/Certificates.tsx
@@ -8,6 +8,7 @@ import image1 from "@/public/certificate-1.png";
 import image2 from "@/public/certificate-2.png";
 import image3 from "@/public/certificate-3.png";
 import image4 from "@/public/certificate-4.png";
+import image5 from "@/public/certificate-5.png";
 
 const blackSansFont = blackSans({ src: "../../../public/Blacker-Sans.ttf" });
 
@@ -71,6 +72,23 @@ export default async function Certificates(): Promise<JSX.Element> {
               >
                 <Image
                   src={image3}
+                  alt="Certificate image"
+                  style={{
+                    width: "200px",
+                    height: "auto",
+                  }}
+                />
+              </Link>
+            </div>
+          </RevealDesktop>
+          <RevealDesktop>
+            <div className=" justify-self-center">
+              <Link
+                href="https://creative.softuni.bg/certificates/details/51019/178a30fc"
+                target="_blank"
+              >
+                <Image
+                  src={image5}
                   alt="Certificate image"
                   style={{
                     width: "200px",
