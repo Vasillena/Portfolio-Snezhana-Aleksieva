0324781f1987bc85c2e6a6f249df33f4ba1e68cd
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-10-10 21:30:38 +0300

    V

--- app/components/Home/FeaturedProjects.tsx
+++ app/components/Home/FeaturedProjects.tsx
@@ -1,19 +1,15 @@
-import { getCurrentLocale, getI18n } from "@/locales/server";
-
 import Image from "next/image";
 import Link from "next/link";
 import RevealDesktop from "../Common/RevealDesktop";
 import { cn } from "@/lib/utils";
-import image1 from "@/public/projects.svg";
-import image2 from "@/public/fproject-1.png";
-import image3 from "@/public/fproject-2.png";
-import image4 from "@/public/fproject-3.png";
-import image5 from "@/public/fproject-4.png";
-import image6 from "@/public/projects-2.svg";
+import { getI18n } from "@/locales/server";
+import image1 from "@/public/fproject-1.png";
+import image2 from "@/public/fproject-2.png";
+import image3 from "@/public/fproject-3.png";
+import image4 from "@/public/fproject-4.png";
 import { oranienbaum } from "@/lib/fonts";
 
 export default async function FeaturedProjects(): Promise<JSX.Element> {
-  const locale = await getCurrentLocale();
   const t = await getI18n();
 
   return (
@@ -28,20 +24,18 @@ export default async function FeaturedProjects(): Promise<JSX.Element> {
           >
             {t("home.title-3")}
           </h2>
-          {/* <Image
-            src={locale === "en" ? image1 : image6}
-            alt="Text image"
-            className={locale === "en" ? "mt-[28px] sm:mt-[30px]" : "mt-[10px]"}
-            style={{
-              width: locale === "en" ? "128px" : "197px",
-              height: "auto",
-              alignSelf: "flex-end",
-              marginLeft: "-36px",
-              zIndex: 1,
-            }}
-          /> */}
         </div>
         <div className="mx-auto flex flex-wrap justify-center xl:justify-between gap-y-20 gap-x-20 xl:gap-x-0 mt-8">
+          <RevealDesktop>
+            <div className="w-[300px] h-[300px] relative flex justify-center">
+              <Image
+                src={image1}
+                alt="Project image"
+                fill
+                object-fit="contain"
+              />
+            </div>
+          </RevealDesktop>
           <RevealDesktop>
             <div className="w-[300px] h-[300px] relative flex justify-center">
               <Image
@@ -52,6 +46,7 @@ export default async function FeaturedProjects(): Promise<JSX.Element> {
               />
             </div>
           </RevealDesktop>
+
           <RevealDesktop>
             <div className="w-[300px] h-[300px] relative flex justify-center">
               <Image
@@ -62,21 +57,10 @@ export default async function FeaturedProjects(): Promise<JSX.Element> {
               />
             </div>
           </RevealDesktop>
-
           <RevealDesktop>
             <div className="w-[300px] h-[300px] relative flex justify-center">
               <Image
                 src={image4}
-                alt="Project image"
-                fill
-                object-fit="contain"
-              />
-            </div>
-          </RevealDesktop>
-          <RevealDesktop>
-            <div className="w-[300px] h-[300px] relative flex justify-center">
-              <Image
-                src={image5}
                 alt="Project image"
                 fill
                 object-fit="contain"
