cacff22c70d5514d68f8650e3cfc089b5dab47d1
Vasilena Yoneva <vassito@abv.bg>
2026-01-25 17:46:49 +0200

    Update useFollowPointer.tsx

--- lib/useFollowPointer.tsx
+++ lib/useFollowPointer.tsx
@@ -3,7 +3,9 @@ import { frame, useMotionValue, useSpring } from "framer-motion";
 
 const spring = { damping: 50, stiffness: 500, restDelta: 0.0001 };
 
-export function useFollowPointer(ref: RefObject<HTMLElement>) {
+export function useFollowPointer<T extends HTMLElement>(
+  ref: RefObject<T | null>
+) {
   const x = useSpring(useMotionValue(0), spring);
   const y = useSpring(useMotionValue(0), spring);
 
